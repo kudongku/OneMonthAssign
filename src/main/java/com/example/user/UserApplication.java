075c2462d1d48c92b83914f57e16f45a90d58046
kudongku <kudongku@naver.com>
2024-07-30 21:39:27 +0900

    :sparkles: feat : swagger 적용

--- src/main/java/com/example/user/UserApplication.java
+++ src/main/java/com/example/user/UserApplication.java
@@ -1,9 +1,11 @@
 package com.example.user;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@OpenAPIDefinition(info = @Info(title = "API 명세서", version = "v1"))
 @SpringBootApplication
 public class UserApplication {
 
--- src/main/java/com/example/user/controller/UserController.java
+++ src/main/java/com/example/user/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.user.controller;
 import com.example.user.dto.UserSignupRequestDto;
 import com.example.user.dto.UserSignupResponseDto;
 import com.example.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/v1/users")
+@Tag(name = "UserController", description = "회원가입을 포함한 컨트롤러입니다.")
 @RestController
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "업체 회원가입", description = "업체 측에서 회원가입 할 때 사용하는 API")
     @PostMapping("/signup")
     public UserSignupResponseDto signup(@RequestBody UserSignupRequestDto requestDto) {
         return userService.signup(requestDto);
